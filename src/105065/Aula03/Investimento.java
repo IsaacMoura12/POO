3b5f69db19164f02fe50b7ba25ad5c3764740657
IsaacMoura12 <isaac.moura@ua.pt>
2023-02-27 17:32:10 +0000

    update

---
+++ src/105065/Aula03/Investimento.java
@@ -0,0 +1,20 @@
+package Aula03;
+import util.*;
+
+
+public class Investimento {
+    public static void main(String[] args) {
+        double montante = Utils.input_mul("Montante (positivo e múltiplo de 1000):", 0, 1000);
+        double juro = Utils.input_interval(" Juro ( entre 0 e 5 %):", 0, 5)/100;
+
+        for (int i = 1; i <= 12; i++) {
+            double tmp = montante * juro;
+            montante = tmp + montante;
+            System.out.print("Valor no mes " + i + " ");
+            System.out.printf("%.2f", montante);
+            System.out.println();
+            
+        }
+    }
+    
+}
---
+++ src/105065/Aula03/Primos.java
@@ -0,0 +1,22 @@
+package Aula03;
+import Aula02.Input;
+import util.*;
+
+
+public class Primos {
+    public static void main(String[] args) {
+
+    double n = Input.readDouble("Número:");
+    double sum = 0;
+
+    for (int i = 1; i < n; i++) {
+        if(Maths.is_primo(i)== true){
+            sum = sum + i;
+        }
+    }
+    System.out.print("Soma dos primos:" + sum);
+    System.out.println("\n");;
+
+    }
+    
+}
