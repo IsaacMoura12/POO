133c7884e327b724e3526f2a1521151aa2af4554
IsaacMoura12 <isaac.moura@ua.pt>
2023-03-06 14:31:53 +0000

    update

--- src/utils/User_input.java
+++ src/utils/User_input.java
@@ -33,7 +33,7 @@ public class User_input {
         }
 
     
-    // input intervalo
+    // input intervalo double
     public static double input_interval(String msg, double min, double max) {
         double v;
         while(true){
@@ -46,7 +46,7 @@ public class User_input {
         return v;
     }
 
-
+    // input intervalo int
     public static int input_interval_int(String msg, int min, int max) {
         int v;
         while(true){
@@ -73,6 +73,7 @@ public class User_input {
         return v;
     }
 
+    // input string 
     public static String input_str(String msg){
         String v;
         while(true){
@@ -80,10 +81,89 @@ public class User_input {
             v = sc.nextLine();
             if(v.isEmpty()== false) break;
                 System.out.println("String Vazia!");
-            
         }
         return v;
+    }
 
+    // contador de numeros numa string
+    public static int countDigits(String s){
+        int count = 0;
+        for (int i = 0; i <= s.length()-1; i++) {
+            if (Character.isDigit(s.charAt(i)))
+                count = count +1;
+        }
+        return count;
+    }
+
+
+    // contador de espaços numa String
+    public static int countSpaces(String s){
+        int count = 0;
+        String tmp = s;
+        char ch[] = tmp.toCharArray();
+        for (int i = 0; i <= ch.length-1; i++) {
+            if(ch[i] == ' ')
+            count = count +1;
+        }
+        return count;
+    }
+
+    //verifica se String só tem minúsculas
+    public static boolean is_allLower(String s){
+        boolean status;
+        int count = 0;
+        String tmp = s;
+        char ch[] = tmp.toCharArray();
+        for (int i = 0; i <= ch.length-1; i++) {
+            if(Character.isUpperCase(ch[i]))
+            count = count +1;
+        }
+        if(count>0){
+            status = false;
+
+        }else{
+            status = true;
+        }
+        return status;
+    }
+
+
+    // verifica se String só tem maiúsculas
+    public static boolean is_allUpper(String s){
+        boolean status;
+        int count = 0;
+        String tmp = s;
+        char ch[] = tmp.toCharArray();
+        for (int i = 0; i <= ch.length-1; i++) {
+            if(Character.isLowerCase(ch[i]))
+            count = count +1;
+        }
+        if(count>0){
+            status = false;
+
+        }else{
+            status = true;
+        }
+        return status;
+    }
+
+    // junta os espaços numa String
+    public static String removeSpaces(String s){
+        String new_s = s.trim().replaceAll(" +", " ");
+        return new_s;
+    }
+
+    // verifica se String é Palindromo
+    public static boolean isPalindrome(String s){
+        String s_reverse = "";
+        for (int i = s.length()-1; i >= 0; i--) {
+            s_reverse = s_reverse + s.charAt(i);   
+        }
+        if(s.toLowerCase().equals(s_reverse.toLowerCase())){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
