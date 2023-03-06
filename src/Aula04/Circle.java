838355bbc412b86205056a09cb2fb9b1ed774ede
IsaacMoura12 <isaac.moura@ua.pt>
2023-03-06 16:17:34 +0000

    update

---
+++ src/Aula04/Circle.java
@@ -0,0 +1,35 @@
+package Aula04;
+
+public class Circle {
+    private double radius;
+
+
+    Circle(double radius){
+        this.setCircle(radius);
+    }
+
+    public void setCircle(double radius){
+        assert radius > 0;
+        this.radius = radius;
+    }
+
+    public String toString(){
+        return "Circle with radius:" + this.radius;
+    }
+    
+    public double perimetro(){
+        return 2 * Math.PI * this.radius;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double area(){
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public boolean equals(Circle b){
+        return this.radius == b.getRadius();
+    }
+}
