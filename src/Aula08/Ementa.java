9bde8add32e682f97a557b98b132ff029f40ac4a
Isaac <isaac.moura@ua.pt>
2023-04-10 22:20:56 +0100

    update

---
+++ src/Aula08/Ementa.java
@@ -0,0 +1,44 @@
+package Aula08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ementa{
+    protected String nome;
+    protected String local;
+    protected List<Prato> ementa;
+
+
+    Ementa(String nome, String local){
+        assert nome.length()>0;
+        assert local.length()>0;
+        this.nome = nome;
+        this.local = local;
+
+        ementa = new ArrayList<>();
+    }
+
+    public void addPrato(Prato prato){
+        ementa.add(prato);
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getLocal(){
+        return local;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nEmenta" + nome + ", " + local);
+        for (Prato prato : ementa){
+            sb.append(prato + "\n");
+        }
+        return sb.toString();
+    }
+
+    
+}
